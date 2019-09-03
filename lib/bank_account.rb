60c531d0a4a0a6c9bd7dc2cbe5732e8fd05cf693
Marc Blanchard <marcblanchard0930@gmail.com>
2019-09-03 02:58:46 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,8 +1,9 @@
 class BankAccount
   
+  attr_accessor :balance, :status
+  attr_reader :name 
   
-  
-  def initialize(name, balance = 100)
+  def initialize(name, balance)
     @name = name 
     @balance = balance 
   end
